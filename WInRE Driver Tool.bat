cffa6921d387a5e1e2df501a387c19055c2280e5
Yonatan Israeli <yonatanreuvenisraeli@gmail.com>
2025-11-04 13:07:36 -0500

    Update WInRE Driver Tool.bat

--- WInRE Driver Tool.bat
+++ WInRE Driver Tool.bat
@@ -494,11 +494,11 @@ goto "Export"
 echo.
 echo Overwriting Windows Recovery Environment.
 del "%WinREPath%\winre.wim" /f /q > nul 2>&1
-copy "%SystemDrive%\winre.wim" "%WinREPath%\winre.wim" /y /v > nul 2>&1
+copy "%SystemDrive%\winre.wim" "%WinREPath% /y /v > nul 2>&1
 del "%SystemDrive%\winre.wim" /f /q > nul 2>&1
 echo Windows Recovery Environment overwritten.
-if /i "%WinREAsk%"=="Yes" goto "RemoveLetter"
-if /i "%WinREAsk%"=="No" goto "Start"
+if /i "%WinREAsk%"=="Yes" goto "Start"
+if /i "%WinREAsk%"=="No" goto "RemoveLetter"
 
 :"RemoveLetter"
 if exist "diskpart.txt" goto "DiskPartExistRemoveLetter"
