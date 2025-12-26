936bcf751720c16d0101e1af53770f7a655cde30
Yonatan Israeli <yonatanreuvenisraeli@gmail.com>
2025-12-25 19:16:35 -0500

    Update WInRE Driver Tool.bat

--- WInRE Driver Tool.bat
+++ WInRE Driver Tool.bat
@@ -2,7 +2,7 @@
 title WinRE Driver Tool
 setlocal
 echo Program Name: WinRE Driver Tool
-echo Version: 1.1.1
+echo Version: 1.1.2
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
@@ -22,7 +22,7 @@ goto "Close"
 :"InWindowsPreinstallationEnvironmentWindowsRecoveryEnvironment"
 echo.
 echo You are in Windows Preinstallation Environment or Windows Recovery Environment! You must run this batch file in Windows. Press any key to close this batch file.
-pause > nul 2>&1
+pause > nul 
 goto "Close"
 
 :"Start"
@@ -56,7 +56,7 @@ if exist "diskpart.txt" goto "DiskPartExistVolume"
 echo Listing volumes attached to this PC.
 (echo list vol) > "diskpart.txt"
 (echo exit) >> "diskpart.txt"
-"%windir%\System32\diskpart.exe" /s "diskpart.txt" 2>&1
+"%windir%\System32\diskpart.exe" /s "diskpart.txt"
 if not "%errorlevel%"=="0" goto "VolumeError"
 del "diskpart.txt" /f /q > nul 2>&1
 echo Volumes attached to this PC listed.
@@ -377,7 +377,7 @@ goto "2"
 :"AddDriver"
 echo.
 echo Adding driver file(s) to Windows Recovery Environment.
-"%windir%\System32\Dism.exe" /Image:"%SystemDrive%\Mount" /Add-Driver /Driver:"%DriverPath%" 2>&1
+"%windir%\System32\Dism.exe" /Image:"%SystemDrive%\Mount" /Add-Driver /Driver:"%DriverPath%"
 if /i not "%errorlevel%"=="0" goto "Error2"
 echo Driver file(s) added to Windows Recovery Environment.
 goto "AddAnotherDriver"
@@ -397,7 +397,7 @@ echo Invalid syntax!
 goto "AddAnotherDriver"
 
 :"3"
-"%windir%\System32\Dism.exe" /Get-Drivers /Image:"%SystemDrive%\Mount" 2>&1
+"%windir%\System32\Dism.exe" /Get-Drivers /Image:"%SystemDrive%\Mount"
 if /i not "%errorlevel%"=="0" goto "Error3"
 echo.
 set DriverName=
@@ -416,7 +416,7 @@ goto "SureDriverName"
 :"RemoveDriver"
 echo.
 echo Removing %DriverName% from Windows Recovery Environment.
-"%windir%\System32\Dism.exe" /Image:"%SystemDrive%\Mount" /Remove-Driver /Driver:%DriverName% 2>&1
+"%windir%\System32\Dism.exe" /Image:"%SystemDrive%\Mount" /Remove-Driver /Driver:%DriverName%
 if /i not "%errorlevel%"=="0" goto "Error3"
 echo %DriverName% removed from Windows Recovery Environment.
 goto "RemoveAnotherDriver"
